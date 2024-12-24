bfccf42039ecbed63d0ca82a9b9f8f075560ff60
JavierSC4 <salinas.cavas@gmail.com>
2024-12-24 20:49:45 -0300

    Pruebas exitosas de conexión a la base de datos v7

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "pool_pre_ping": True,
+    "pool_recycle": 1800,  # Recycle connections every 30 minutes
     "pool_size": 5,
     "max_overflow": 10,
     "pool_timeout": 30,
